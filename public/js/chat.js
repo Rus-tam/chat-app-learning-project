b6796be576d0aa0efe03a97aa4bafca0bf7dae36
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2022-01-05 21:40:15 +0500

    message acknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,5 +7,28 @@ socket.on('message', (message) => {
 document.querySelector('#message-form').addEventListener('submit', (e) => {
 	e.preventDefault();
 	const message = e.target[0].value;
-	socket.emit('sendMessage', message);
+	socket.emit('sendMessage', message, (error) => {
+		if (error) {
+			return console.log(error);
+		}
+
+		console.log('Message delivered!');
+	});
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+	if (!navigator.geolocation) {
+		return alert('Geolocation is not supported by your browser!');
+	}
+
+	navigator.geolocation.getCurrentPosition((position) => {
+		const location = {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude,
+		};
+
+		socket.emit('sendLocation', location, () => {
+			console.log('Location shared!');
+		});
+	});
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const port = process.env.PORT || 3000;
 
@@ -17,9 +18,26 @@ io.on('connection', (socket) => {
 	console.log('New websocket connection');
 
 	socket.emit('message', 'Welcome!');
+	socket.broadcast.emit('message', 'A new user has joined!');
 
-	socket.on('sendMessage', (message) => {
+	socket.on('sendMessage', (message, callback) => {
+		const filter = new Filter();
+
+		if (filter.isProfane(message)) {
+			return callback('Profanity is not allowed');
+		}
+
 		io.emit('message', message);
+		callback();
+	});
+
+	socket.on('disconnect', () => {
+		io.emit('message', 'A user has left');
+	});
+
+	socket.on('sendLocation', (location, callback) => {
+		io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`);
+		callback();
 	});
 });
 
